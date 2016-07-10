1be6372548a2563da12960b4559a8dbfdddaf165
Amir Suahil <aamirs98@gmail.com>
2016-07-11 00:17:20 +0530

    deleted angular

--- app/js/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('myApp', ['myApp.controllers']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/amir-suhail', {templateUrl: 'partials/home.html', controller: 'homeCtrl'});
-    $routeProvider.otherwise({redirectTo: '/amir-suhail'});
-  }]);
